adde1b877c25529fffd6a02738acff0c00cb1cce
Ben Huang <benxu.huang@gmail.com>
2021-12-29 09:03:18 +0800

    add interface

--- MyMusic.Core/Models/Artist.cs
+++ MyMusic.Core/Models/Artist.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace MyMusic.Core.Models {
     public class Artist {
---
+++ MyMusic.Core/Repositories/IArtistRepository.cs
@@ -0,0 +1,10 @@
+using MyMusic.Core.Models;
+
+namespace MyMusic.Core.Repositories
+{
+    public interface IArtistRepository : IRepository<Artist>
+    {
+        Task<IEnumerable<Artist>> GetAllWithMusicsAsync();
+        Task<Artist> GetWithMusicsByIdAsync(int id);
+    }
+}
