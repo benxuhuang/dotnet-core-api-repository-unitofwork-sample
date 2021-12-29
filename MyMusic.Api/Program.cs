a309b49ebee5fc7a7ced9c626b7845050139f48f
Ben Huang <benxu.huang@gmail.com>
2021-12-29 13:58:16 +0800

    add repository and unitofwork

--- MyMusic.Api/Program.cs
+++ MyMusic.Api/Program.cs
@@ -1,7 +1,18 @@
+using MyMusic.Core;
+using MyMusic.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+// var provider = builder.Services.BuildServiceProvider();
+// var configuration = provider.GetService<IConfiguration>();
+var connStr = builder.Configuration.GetValue<string>("ConnectionStrings:Default");
+
+builder.Services.AddDbContext<MyMusicDbContext>(options =>
+ options.UseSqlServer(connStr,
+  x => x.MigrationsAssembly("MyMusic.Data")));
 
 // Add services to the container.
-
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
