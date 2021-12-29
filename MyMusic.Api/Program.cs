8ae977dca3442f12b5b79feb411805a0ef0f7472
Ben Huang <benxu.huang@gmail.com>
2021-12-29 15:31:11 +0800

    add service

--- MyMusic.Api/Program.cs
+++ MyMusic.Api/Program.cs
@@ -1,17 +1,23 @@
 using MyMusic.Core;
 using MyMusic.Data;
 using Microsoft.EntityFrameworkCore;
+using MyMusic.Services;
+using MyMusic.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure the connection of database
 var connStr = builder.Configuration.GetValue<string>("ConnectionStrings:Default");
-
 builder.Services.AddDbContext<MyMusicDbContext>(options =>
  options.UseSqlServer(connStr,
   x => x.MigrationsAssembly("MyMusic.Data")));
 
-// Add services to the container.
+// Configure Dependency injection
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IMusicService, MusicService>();
+builder.Services.AddScoped<IArtistService, ArtistService>();
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
