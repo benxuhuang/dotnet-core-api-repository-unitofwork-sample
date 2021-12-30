97555f90ec6278f451de2a62202b67501f5d938a
Ben Huang <benxu.huang@gmail.com>
2021-12-30 10:31:10 +0800

    add artist controller

--- MyMusic.Api/Controllers/MusicsController.cs
+++ MyMusic.Api/Controllers/MusicsController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using MyMusic.Api.Resources;
+using MyMusic.Api.Validations;
 using MyMusic.Core.Models;
 using MyMusic.Core.Services;
 
@@ -55,5 +56,53 @@ namespace MyMusic.Api.Controllers
 
             return NoContent();
         }
+
+        [HttpPost("")]
+        public async Task<ActionResult<MusicResource>> CreateMusic([FromBody] SaveMusicResource saveMusicResource)
+        {
+            //validate
+            var validator = new SaveMusicResourceValidator();
+            var validatorResult = await validator.ValidateAsync(saveMusicResource);
+            if (!validatorResult.IsValid)
+                return BadRequest(validatorResult.Errors);
+
+            //create music
+            var musicToCreate = _mapper.Map<SaveMusicResource, Music>(saveMusicResource);
+            var newMusic = await _musicService.CreateMusic(musicToCreate);
+
+            //get music
+            var music = await _musicService.GetMusicById(newMusic.Id);
+            var musicResource = _mapper.Map<Music, MusicResource>(music);
+
+            return Ok(musicResource);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<MusicResource>> UpdateMusic(int id, [FromBody] SaveMusicResource saveMusicResource)
+        {
+            //validate
+            var validator = new SaveMusicResourceValidator();
+            var validatorResult = await validator.ValidateAsync(saveMusicResource);
+
+            var requestIsInValid = id == 0 || !validatorResult.IsValid;
+            if (requestIsInValid)
+                return BadRequest(validatorResult.Errors);
+
+            //get old music
+            var musicToUpdate = await _musicService.GetMusicById(id);
+            if (musicToUpdate == null)
+                return NotFound();
+
+            //update old music
+            var music = _mapper.Map<SaveMusicResource, Music>(saveMusicResource);
+            await _musicService.UpdateMusic(musicToUpdate, music);
+
+            //get updated music
+            var updatedMusic = await _musicService.GetMusicById(id);
+            var updatedMusicResource = _mapper.Map<Music, MusicResource>(updatedMusic);
+
+            return Ok(updatedMusic);
+        }
+
     }
 }
